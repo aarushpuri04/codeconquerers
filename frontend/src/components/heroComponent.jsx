242d5c2f14183e5aa3635ccc2e934c3a683574a5
Sparsh Agarwal <sparshaga913@gmail.com>
2024-08-01 23:28:10 +0530

    minor changes

--- frontend/src/components/heroComponent.jsx
+++ frontend/src/components/heroComponent.jsx
@@ -2,7 +2,7 @@ export default function HeroComponent({ imageSrc, text, btnText }) {
     return (
   
       <div className="relative bg-cover bg-center bg-no-repeat h-[45rem]">
-        <img src={imageSrc} alt="" className="absolute inset-0 object-fill w-full h-full" />
+        <img src={imageSrc} alt="" className="absolute inset-0 object-fill w-[100vw] h-full " />
         <div className="relative inset-0 flex pt-48 justify-center">
           <h1 className="text-white text-[4rem] font-bold max-w-[55rem] text-center">{text}</h1>
         </div>
